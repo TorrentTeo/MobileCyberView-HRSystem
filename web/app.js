824488621598d0595fa3ac39a368aa844780375d
Lee Re <leere@sd.taylors.edu.my>
2021-01-21 03:03:58 +0800

    getpost

--- web/app.js
+++ web/app.js
@@ -2,10 +2,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const router = express.Router()
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+// const connect = require("connect");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Static files
+
 app.use(express.static('public'))
+app.use("/", router);
+
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
@@ -21,6 +30,21 @@ app.get('', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login')
 })
+
+
+
+//GET
+router.get('/destination', (req, res) => {
+    res.render('destination', {output: "test"})
+})
+
+//POST
+router.post('/login', (req, res) => {
+    var {username, password} = req.body;
+    console.log(req.body);
+    res.redirect('/destination');
+})
+
 
 //listen on port
 app.listen(port, () => console.info('Listening on port ' + port))
