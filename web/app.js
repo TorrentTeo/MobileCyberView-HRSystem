083824ffc7da02442c0f55ed328cc8ff2ac8d1a5
hideki212 <dekiyana@gmail.com>
2021-01-21 03:04:02 +0800

    Some Log in done

--- web/app.js
+++ web/app.js
@@ -2,25 +2,54 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
+const router = express.Router();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Static files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
+app.use("/", router);
 
-//Set Views
+app.use(cookieParser());
 app.set('views', './web')
 app.set('view engine', 'ejs')
 
-app.get('', (req, res) => {
+router.get('', (req, res) => {
     res.render('index')
 })
 
-app.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
     res.render('login')
 })
 
+router.post('/login', (req, res) => {
+    var { username, pass } = req.body;
+    if(!username || !pass)
+        res.render('login',{ error: true, message: "Username & password are required" })
+    axios.post('http://localhost:5000/api/auth/login', {
+        email: username,
+        password: pass
+    }).then((response) => {
+        console.log(`statusCode: ${response.statusCode}`)
+        console.log(response)
+    }).catch((error) => {
+        console.error(error)
+    })
+    res.redirect('/dashboard');
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard', {output: "test"});
+})
 //listen on port
 app.listen(port, () => console.info('Listening on port ' + port))
+
+
+
