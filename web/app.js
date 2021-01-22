6a5eeb07bab84b592f7d06e4e527b71398ab9b23
Lee Re <leere@sd.taylors.edu.my>
2021-01-22 14:45:31 +0800

    update app.js

--- web/app.js
+++ web/app.js
@@ -4,11 +4,15 @@ const app = express()
 const port = 3000
 const router = express.Router()
 const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
-// const connect = require("connect");
+const { urlencoded } = require('body-parser')
+
+const axios = require('axios')
+const cookieParser = require('cookie-parser')
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //Static files
 
@@ -20,6 +24,8 @@ app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
 
 //Set Views
+
+// app.use(cookieParser());
 app.set('views', './web')
 app.set('view engine', 'ejs')
 
@@ -42,8 +48,25 @@ router.get('/destination', (req, res) => {
 router.post('/login', (req, res) => {
     var {username, password} = req.body;
     console.log(req.body);
+    if(!username || !password)
+        res.render('login',{ error: true, message: "Username & password are required" })
+    axios.post('http://localhost:5000/api/auth/login', {
+    // auth: {    
+        email: username,
+        password: password
+    // }
+    }).then((response) => {
+        console.log(`statusCode: ${response.statusCode}`)
+        console.log(response)
+    }).catch((error) => {
+        console.error(error)
+    })
     res.redirect('/destination');
 })
+
+// router.get('/destination', (req, res) => {
+//     res.render('destination', {output: "test"});
+// })
 
 
 //listen on port
