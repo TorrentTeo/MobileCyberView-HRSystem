60de6f01dad7c326f36c34752c97321351dc6c3a
hideki212 <dekiyana@gmail.com>
2021-02-02 23:47:46 +0800

    added some code for the Calendar view

--- web/controllers/calendarController.js
+++ web/controllers/calendarController.js
@@ -6,13 +6,14 @@ exports.getCalendar = async (req, res) => {
     var cookie = get_cookies(req)["authcookie"];
     var data = req.body;
     var headers = {Authorization: "Bearer " + get_cookies(req)["authcookie"]};
+    var newLeaveRequests = [];
     await axios.get(url, {headers: headers} ,data).then((response) => {
         console.log(`statusCode: ${response.data.code}`)
         console.log(response.data)
         var resData = response.data;
         var leaveRequests = resData.results.leave;
         console.log(leaveRequests)
-        var newLeaveRequests = [];
+
         for(key in leaveRequests){
             console.log(leaveRequests[key])
             // var days = datediff(parseDate(new Date(leaveRequests[key].from).toDateString()), parseDate(new Date(leaveRequests[key].to).toDateString()));
@@ -26,10 +27,36 @@ exports.getCalendar = async (req, res) => {
             }
             newLeaveRequests.push(leave)
         }
-        res.render('calendar', {data: {leaveRequests: newLeaveRequests }})
-        return resData;
+
     }).catch((error) => {
         console.log(error)
         return error;
-    })  
+    })
+
+
+    url = "http://localhost:5000/api/employee/Allcalendar";
+    var newCalendarRequests = [];
+    await axios.get(url, {headers: headers} ,data).then((response) => {
+        console.log(`statusCode: ${response.data.code}`)
+        console.log(response.data)
+        var resData = response.data;
+        var calendarRequests = resData.results.calendar;
+        console.log(calendarRequests)
+
+        for(key in calendarRequests){
+            console.log(calendarRequests[key])
+            // var days = datediff(parseDate(new Date(leaveRequests[key].from).toDateString()), parseDate(new Date(leaveRequests[key].to).toDateString()));
+            var calendar = {
+                date: new Date(calendarRequests[key].date).toDateString(),
+                activity: calendarRequests[key].activity,
+                time: new Date(calendarRequests[key].date).toISOString().split("T")[1].split(".")[0]
+            }
+            newCalendarRequests.push(calendar)
+        }
+
+    }).catch((error) => {
+        console.log(error)
+        return error;
+    })       
+    res.render('calendar', {data: {leaveRequests: newLeaveRequests, calendar: newCalendarRequests }})
 }
